c28be1c0877375f8828de6e4bef121a8ec3283e3
Kyle Ribordy <kdribordy@gmail.com>
2013-03-18 19:55:25 -0400

    Added New User page

--- Server/newUser.php
+++ Server/newUser.php
@@ -1,1 +1,17 @@
-hihi
+<?php
+
+  include 'database.php';
+  
+  $user_name = $_GET["user_name"];
+  $password = $_GET["password"];
+  
+  $token = md5($user_name+$password+"666");
+  
+  $result = $mysqli->query("INSERT INTO users (username,password,token) VALUES('$user_name','$password','$token')");  
+  
+  if($result==false) {
+    $error = "NEW_USER_FAILURE";
+    include 'error.php';
+  }
+  
+?>
