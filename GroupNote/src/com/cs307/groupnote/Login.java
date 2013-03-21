70bb3930f4404d0432185d238d2ae73ba1fab348
Justin Imel <jimel@purdue.edu>
2013-03-21 19:38:18 -0400

    Committing for Spencer
    Added login and settings pages.  The pages are also linked into the
    application.

--- GroupNote/src/com/cs307/groupnote/Login.java
+++ GroupNote/src/com/cs307/groupnote/Login.java
@@ -2,7 +2,10 @@ package com.cs307.groupnote;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.app.Activity;
+import android.content.Intent;
 
 public class Login extends Activity {
 
@@ -10,6 +13,16 @@ public class Login extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        
+      //create button
+        final Button button = (Button) findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Intent i = new Intent(getBaseContext(), MainActivity.class);
+            	startActivity(i);
+            }
+        });
     }
 
     
